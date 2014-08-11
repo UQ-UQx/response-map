925cf7622c7bc3666d73a44e466453d2309631c4
Steven Chen <ejsteven@gmail.com>
2014-08-11 17:34:10 +1000

    Only send score if enabled

--- map.php
+++ map.php
@@ -1,63 +1,65 @@
 <?php
-	// Give participation mark
-	$student_grade = 1;
+	if (!empty($_SESSION[$_POST['lis_result_sourcedid']]['lis_outcome_service_url'])) {
+		// Give participation mark
+		$student_grade = 1;
 
-	$grade_message = 'Thanks for responding. You have been given participation mark.';
+		$grade_message = 'Thanks for responding. You have been given participation mark.';
 
-	$outcome_url = $_SESSION[$_POST['lis_result_sourcedid']]['lis_outcome_service_url'];
-	$consumer_key = $_SESSION[$_POST['lis_result_sourcedid']]['oauth_consumer_key'];
-	$consumer_secret = '123456';
+		$outcome_url = $_SESSION[$_POST['lis_result_sourcedid']]['lis_outcome_service_url'];
+		$consumer_key = $_SESSION[$_POST['lis_result_sourcedid']]['oauth_consumer_key'];
+		$consumer_secret = '123456';
 
-	$hmac_method = new OAuthSignatureMethod_HMAC_SHA1();
+		$hmac_method = new OAuthSignatureMethod_HMAC_SHA1();
 
-	$body = '<?xml version = "1.0" encoding = "UTF-8"?>
-	<imsx_POXEnvelopeRequest xmlns = "http://www.imsglobal.org/services/ltiv1p1/xsd/imsoms_v1p0">
-		<imsx_POXHeader>
-			<imsx_POXRequestHeaderInfo>
-			<imsx_version>V1.0</imsx_version>
-			<imsx_messageIdentifier>' . $_SERVER['REQUEST_TIME'] . '</imsx_messageIdentifier>
-		</imsx_POXRequestHeaderInfo>
-	</imsx_POXHeader>
-	<imsx_POXBody>
-		<replaceResultRequest>
-			<resultRecord>
-				<sourcedGUID>
-					<sourcedId>' . $_POST['lis_result_sourcedid'] . '</sourcedId>
-				</sourcedGUID>
-				<result>
-					<resultScore>
-						<language>en</language>
-						<textString>' . $student_grade . '</textString>
-					</resultScore>
-				</result>
-			</resultRecord>
-		</replaceResultRequest>
-	</imsx_POXBody>
-</imsx_POXEnvelopeRequest>';
+		$body = '<?xml version = "1.0" encoding = "UTF-8"?>
+		<imsx_POXEnvelopeRequest xmlns = "http://www.imsglobal.org/services/ltiv1p1/xsd/imsoms_v1p0">
+			<imsx_POXHeader>
+				<imsx_POXRequestHeaderInfo>
+					<imsx_version>V1.0</imsx_version>
+					<imsx_messageIdentifier>' . $_SERVER['REQUEST_TIME'] . '</imsx_messageIdentifier>
+				</imsx_POXRequestHeaderInfo>
+			</imsx_POXHeader>
+			<imsx_POXBody>
+				<replaceResultRequest>
+					<resultRecord>
+						<sourcedGUID>
+							<sourcedId>' . $_POST['lis_result_sourcedid'] . '</sourcedId>
+						</sourcedGUID>
+						<result>
+							<resultScore>
+								<language>en</language>
+								<textString>' . $student_grade . '</textString>
+							</resultScore>
+						</result>
+					</resultRecord>
+				</replaceResultRequest>
+			</imsx_POXBody>
+		</imsx_POXEnvelopeRequest>';
 
-	$hash = base64_encode(sha1($body, TRUE));
-		$params = array('oauth_body_hash' => $hash);
-	$token = '';
-	$content_type = 'application/xml';
+		$hash = base64_encode(sha1($body, TRUE));
+			$params = array('oauth_body_hash' => $hash);
+		$token = '';
+		$content_type = 'application/xml';
 
-	$hmac_method = new OAuthSignatureMethod_HMAC_SHA1();
-	$consumer = new OAuthConsumer($consumer_key, $consumer_secret);
-	$outcome_request = OAuthRequest::from_consumer_and_token($consumer, $token, 'POST', $outcome_url, $params);
-	$outcome_request->sign_request($hmac_method, $consumer, $token);
+		$hmac_method = new OAuthSignatureMethod_HMAC_SHA1();
+		$consumer = new OAuthConsumer($consumer_key, $consumer_secret);
+		$outcome_request = OAuthRequest::from_consumer_and_token($consumer, $token, 'POST', $outcome_url, $params);
+		$outcome_request->sign_request($hmac_method, $consumer, $token);
 
-	$header = $outcome_request->to_header();
-	$header = $header . "\r\nContent-type: " . $content_type . "\r\n";
-	$options = array(
-		'http' => array(
-			'method' => 'POST',
-			'content' => $body,
-			'header' => $header,
-		),
-	);
+		$header = $outcome_request->to_header();
+		$header = $header . "\r\nContent-type: " . $content_type . "\r\n";
+		$options = array(
+			'http' => array(
+				'method' => 'POST',
+				'content' => $body,
+				'header' => $header,
+			),
+		);
 
-	$ctx = stream_context_create($options);
-	$fp = @fopen($outcome_url, 'rb', FALSE, $ctx);
-	$response = @stream_get_contents($fp);
+		$ctx = stream_context_create($options);
+		$fp = @fopen($outcome_url, 'rb', FALSE, $ctx);
+		$response = @stream_get_contents($fp);
+	}
 ?>
 
 <html>
